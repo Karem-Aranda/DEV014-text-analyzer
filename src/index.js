617cff59318d41e6bed6c49b13b5e9dd57d284f3
Karem Aranda <karemara@Air-de-Karem.wind3.hub>
2024-03-04 16:48:08 +0100

    agregue evento al imput y arregle errores del html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textArea = document.querySelector('textarea[name="user-imput"]');
+console.log(textArea)
+
+
+//agregar evento - imput al textarea
+//que dispare un console.log
+
+document.getElementById("textarea").addEventListener("input", myfunction);
+
+function myfunction(textarea) {
+    console.log(textarea.target.value)
+}
+// let text = document.getElementById("input").value;
+
